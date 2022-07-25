d2c2d0bddc3ecc5a52e5b8c1b867a3f0439fa60a
xinxin8816 <8501725+xinxin8816@users.noreply.github.com>
2022-07-25 13:29:49 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+import os
+print('==========================================')
+print('REMOVE all photos with same name as movies')
+print('==========================================')
+print('Move this app to target directory or input full path under here, and press Enter to start.')
+path = input('Path:')
+
+if not path:
+    path = './'
+
+movie_extension = ["mp4", "avi", "rmvb", "wmv", "mov", "mkv", "flv", "ts", "webm", "mpeg", "mpg", "vob"]
+
+for folderName, folders, files in os.walk('./'):
+    for file in files:
+        filename = os.path.splitext(file)[0]
+        suffix = os.path.splitext(file)[1].replace('.', '')
+        if suffix in movie_extension:
+            try:
+                os.remove(os.path.join(folderName, filename + '.jpg'))
+                print('Found:', file, 'deleted same name photo')
+            except FileNotFoundError:
+                print('Found:', file, 'skip')
+
+print('Finished!')
