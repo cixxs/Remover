10e7b6e356967918e7e36f28c440f0cb070850f2
xinxin8816 <8501725+xinxin8816@users.noreply.github.com>
2022-07-26 23:32:45 +0800

    NEW logi

--- main.py
+++ main.py
@@ -8,20 +8,29 @@ path = input('Path:')
 
 if not path:
     path = './'
-
+path = os.path.normpath(path)
 print(path)
 
-movie_extension = ["mp4", "avi", "rmvb", "wmv", "mov", "mkv", "flv", "ts", "webm", "mpeg", "mpg", "vob"]
+movie_extension = [".mp4", ".avi", ".rmvb", ".wmv", ".mov", ".mkv", ".flv", ".ts", ".webm", ".mpeg", ".mpg", ".vob"]
 
 for folderName, folders, files in os.walk(path):
     for file in files:
-        filename = os.path.splitext(file)[0]
-        suffix = os.path.splitext(file)[1].replace('.', '')
-        if suffix in movie_extension:
-            try:
-                os.remove(os.path.join(folderName, filename + '.jpg'))
-                print('Found:', file, 'deleted same name photo')
-            except FileNotFoundError:
-                print('Found:', file, 'skip')
+        #filename = os.path.splitext(file)[0]
+        #suffix = os.path.splitext(file)[1].replace('.', '')
+
+        for suffix in movie_extension:
+            if suffix in file:
+                print('Found:', file)
+                filename = os.path.splitext(file)[0]
+                if os.path.exists(os.path.join(folderName, filename + '.jpg')):
+                    os.remove(os.path.join(folderName, filename + '.jpg'))
+                    print('Del:', filename + '.jpg')
+
+        #if suffix in movie_extension:
+                #    try:
+                #        os.remove(os.path.join(folderName, filename + '.jpg'))
+                #    print('Found:', file, 'deleted same name photo')
+                #    except FileNotFoundError:
+                #    print('Found:', file, 'skip')
 
 print('Finished!')
