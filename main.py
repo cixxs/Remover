bd872f94f710e9ad6d33a6176ed350fc2fd02a90
Zealer <xinxin8816@gmail.com>
2022-08-21 00:07:10 +0800

    Using TK

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
 import os, sys
+from time import sleep
+from tkinter import filedialog, Tk
+
+def choose_directory():
+    directory_root = Tk()
+    directory_root.withdraw()
+    path_work = filedialog.askdirectory()
+    if path_work == '':
+        print('Wrong path, choose again please.')
+        sleep(2)
+        return choose_directory()
+    else:
+        # askdirectory 获得是 正斜杠 路径C:/，所以下面要把 / 换成 反斜杠\
+        return path_work.replace('/', os.sep)
 
 print('==========================================')
 print('REMOVE all photos with same name as movies')
 print('==========================================')
 print('Move this app to target directory or input full path under here, and press Enter to start.')
-path = input('Path:')
+path = choose_directory()
 
-if not path:
-    path = './'
-else:
-    path = path.replace('\\','')
-path = os.path.normpath(path)
 print(path)
 
 movie_extension = [".mp4", ".avi", ".rmvb", ".wmv", ".mov", ".mkv", ".flv", ".ts", ".webm", ".mpeg", ".mpg", ".vob"]
